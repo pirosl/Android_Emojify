664a0614fa2f043b852f232330ec4502d48e5e33
Lucian Piros <pirosl@yahoo.com>
2018-02-05 00:23:53 +0000

    Feature: add new Emojifier class - workds with Mobile Vision library

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,14 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+/**
+ * Emojifier class - works with Mobile Vision library
+ */
+public class Emojifier {
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+
+    }
+}
